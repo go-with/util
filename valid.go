33dfd93467c23ed3d0c6449cada2bbe2975e6010
sidbusy <sidbusy@qq.com>
2018-10-22 11:21:34 +0800

    update mobile regular expression

--- valid.go
+++ valid.go
@@ -5,7 +5,7 @@ import (
 )
 
 var (
-	mobileRE = regexp.MustCompile(`^1(3[0-9]|4[57]|5[012356789]|7[016]|8[0-9])\d{8}$`)
+	mobileRE = regexp.MustCompile(`^1(3[0-9]|4[1456789]|5[012356789]|6[6]|7[01345678]|8[0-9]|9[189])\d{8}$`)
 	digitsRE = regexp.MustCompile(`^\d+$`)
 )
 
